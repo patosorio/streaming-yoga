d67f4bffa31006cbdab9181c7eb0ea3ec66bf21f
patOS <patriciaosorio194@gmail.com>
2021-05-02 20:09:48 +0000

    Adding sorting and categories functionality

--- videos/urls.py
+++ videos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_videos, name='videos'),
+    path('<video_id>', views.video_detail, name='video_detail'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,15 +1,68 @@
-from django.shortcuts import render
-from .models import Video
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+
+from .models import Video, Category
 
 # Create your views here.
 
+
 def all_videos(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all videos, including sorting and search queries """
 
     videos = Video.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                videos = videos.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            videos = videos.order_by(sortkey)
+
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            videos = videos.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter search criteria")
+                return redirect(reverse('videos'))
+            
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            videos = videos.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'videos': videos,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'videos.html', context)
+
+
+def video_detail(request, video_id):
+    """ A view to show the video detail """
+
+    video = get_object_or_404(Video, id=video_id)
+
+    context = {
+        'video': video,
+    }
+
+    return render(request, 'video_detail.html', context)
