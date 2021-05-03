3227b01f1e6cde064b4e3a856ea51bec23d09464
patOS <patriciaosorio194@gmail.com>
2021-05-03 21:48:16 +0000

    Add stripe

--- checkout/models.py
+++ checkout/models.py
@@ -48,3 +48,11 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f' {self.membership.name} on order {self.order.order_number}'
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    stripeid = models.CharField(max_length=255)
+    stripe_subscription_id = models.CharField(max_length=255)
+    cancel_at_period_end = models.BooleanField(default=False)
+    membership = models.BooleanField(default=False)
--- checkout/views.py
+++ checkout/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
+from django.contrib.auth.models import User
 from .forms import OrderForm
-
-def checkout(request):
-    """ a View that renders the bag contents page """
-    return render (request, 'checkout.html')
+from .models import Order, OrderLineItem, Customer
+import stripe
 
 
-def add_to_checkout(request, membership_name):
+
+def checkout(request, membership_type):
     
-    redirect_url = request.POST.get('redirect')
-    membership_name = request.POST.get('membership_name')
+    stripe.api_key = "pk_test_51ImzTfGFCpq2XfOb6Bpz00D7omwfZVYzsc48m2gCyuBWqCssVyl1aW5ZL6COJBXBTM6VSFRKNPJFEmm9QBJ7dfJQ00B9WvNlP6"
+    
+    if membership_type != 'Monthly':
+        membership_id = 'prod_JPpLp6Qm7n2H4F'
+    else:
+        membership_id = 'prod_JPpLoEcXPCvx31'
+                
+        session = stripe.checkout.Session.create(
+            success_url="success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url="cancel?session_id={CHECKOUT_SESSION_ID}",
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price": membership_id,
+                    "quantity": 1,
+                },
+            ],
+            mode="subscription",
+            client_reference_id=request.user,         
+        )
+        return render(request, 'checkout.html', {'session_id': session.id})  
+    return render(request, 'checkout.html')
 
-    selected_membership = request.session.get('selected_membership', {})
 
+def success(request):
+    if request.method == 'GET' and 'session_id' in request.GET:
+        session = stripe.checkout.Session.retrieve(request.GET['session_id'],)
+        customer = Customer()
+        customer.user = request.user
+        customer.stripeid = session.customer
+        customer.membership = True
+        customer.cancel_at_period_end = False
+        customer.stripe_subscription_id = session.subscription
+        customer.save()
+    return render(request, 'membership/success.html')
